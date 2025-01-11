3dd8ecffd339b41719c59cf7433f0fe27e49a85d
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2025-01-11 23:39:15 +0000

    created the Calculations model for my DbContext

---
+++ Models/Calculations.cs
@@ -0,0 +1,10 @@
+﻿namespace JG.Task.Addition.Models
+{
+    public class Calculations
+    {
+        public int Id { get; set; }
+        public decimal FirstNumber { get; set; }
+        public decimal SecondNumber { get; set; }
+        public int Result { get; set; }
+    }
+}
