5b72d42eca227229bbfc77bbb571a828ffc3f32a
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2025-01-12 11:51:22 +0000

    added the dependancy injection for the DbContext into the controller

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JG.Task.Addition.Models;
+using JG.Task.Addition.Data;
+using System.Threading.Tasks;
+
+namespace JG.Task.Addition.Controllers
+{
+    [ApiController]
+
+    public class CalculatorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CalculatorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
